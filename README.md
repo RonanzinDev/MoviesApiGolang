5176e6640b911972c11bf255de8c9f3740bb2062
RonanzinDev <morellianogm@gmail.com>
2021-07-22 17:14:43 -0300

    Finalização do projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+# API COM GOLANG
+
+<br/>
+
+Contruindo uma api simple de filme e diretores sem nenhum banco de dados, apenas com slice para fins de estudo utilizando a linguagem Go do google
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	//Setando headaer para json
+	w.Header().Set("Content-Type", "application/json")
+	// Transformando os movies em json
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			//Não sei como, mas isso ira deletar
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // similar ao req.params
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// setando o cotent type para json
+	w.Header().Set("Content-Type", "application/json")
+	// pegando os paremtros params
+	params := mux.Vars(r)
+	// lfazendo um loop
+	// deletando o movie com o id que esta vindo dos paramentros
+	//adicionando movies
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie 1", Director: &Director{FirstName: "Jonh", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "2312312", Title: "Movie 2", Director: &Director{FirstName: "Gabriel", LastName: "Morelli"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting serve on port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
